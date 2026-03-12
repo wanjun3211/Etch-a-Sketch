1c859e0b54e214fe3120de9fc86c28468d8754ff
Vic <123456wangtom@gmail.com>
2026-03-12 17:58:49 +1300

    Found the solution to make the grid adjust its size acoorind to its nunber

--- javascript.js
+++ javascript.js
@@ -1,13 +1,13 @@
 // create 16*16 grid
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 10; i++) {
     // create sigle div for each row
     const row = document.createElement("div");
     row.classList.toggle("row");
     container.appendChild(row);
 
     // create 16 grids for each row 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 10; i++) {
         const grid = document.createElement("div");
         grid.classList.toggle("grid");
         row.appendChild(grid);
