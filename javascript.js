6e63ebb84d7eda17b288a87773a7e9ad49e969f5
Vic <123456wangtom@gmail.com>
2026-03-13 16:52:27 +1300

    Basic function of this app is finished

--- javascript.js
+++ javascript.js
@@ -1,28 +1,34 @@
 // create 16*16 grid
-const container = document.querySelector(".container");
-for (let i = 0; i < 10; i++) {
+let container = document.querySelector(".container");
+const body = document.querySelector("body");
+
+//set default grid quantity
+let numberOfGrid = 16;
+
+createGrid(numberOfGrid);
+
+
+function createGrid(gridNumber) {
+    for (let i = 0; i < gridNumber; i++) {
     // create sigle div for each row
     const row = document.createElement("div");
     row.classList.toggle("row");
     container.appendChild(row);
 
     // create 16 grids for each row 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < gridNumber; i++) {
         const grid = document.createElement("div");
         grid.classList.toggle("grid");
         row.appendChild(grid);
     }
 }
 
-
+// left trails when mouse hover and leave the grid 
 const all_grids = document.querySelectorAll(".grid");
-
-
-const onMouseOver = (e) =>{
+const onMouseOver = (e) => {
     console.log(e.target);
     e.target.style["background-color"] = "black";
 }
-
 const onMouseOut = (e) =>
     e.target.style["background-color"] = "black";
 
@@ -31,9 +37,64 @@ all_grids.forEach((grid) => {
     grid.addEventListener("mouseout", onMouseOut);
 });
 
+}
 
 
 
 
+function enterNumberOfGrid() {
+    let inputNotCorrect = true;
+    while (inputNotCorrect) {
+        const userInput = +prompt("Please enter the number of grid on one side", "");
+        if (userInput < 100) {
+            numberOfGrid = userInput;
+            inputNotCorrect = false;
+        }
+    }
+
+    body.removeChild(container);
+
+    container = document.createElement("div");
+    container.classList.toggle("container");
+    body.appendChild(container);
+
+
+
+    for (let i = 0; i < numberOfGrid; i++) {
+        // create sigle div for each row
+        const row = document.createElement("div");
+        row.classList.toggle("row");
+        container.appendChild(row);
+
+        // create corresponding grids for each row 
+        for (let i = 0; i < numberOfGrid; i++) {
+            const grid = document.createElement("div");
+            grid.classList.toggle("grid");
+            row.appendChild(grid);
+        }
+    }
+
+
+    const all_grids = document.querySelectorAll(".grid");
+    const onMouseOver = (e) => {
+        console.log(e.target);
+        e.target.style["background-color"] = "black";
+    }
+    const onMouseOut = (e) =>
+        e.target.style["background-color"] = "black";
+
+    all_grids.forEach((grid) => {
+        grid.addEventListener("mouseover", onMouseOver);
+        grid.addEventListener("mouseout", onMouseOut);
+    });
+
+
+
+}
+
+const button = document.querySelector(".button");
+button.addEventListener("click", enterNumberOfGrid);
+
+console.log(numberOfGrid);
 
 
